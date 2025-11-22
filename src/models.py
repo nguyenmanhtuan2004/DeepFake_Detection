1b88bdf85d9e261a40ed77f8171283675896d60a
NguyenManhTuan <nguyentantienblog01@gmail.com>
2025-11-22 20:43:13 +0700

    update MLP

--- src/models.py
+++ src/models.py
@@ -22,18 +22,36 @@ class EfficientNetB3FeatureExtractor(nn.Module):
         return self.model(x)
 
 class MetaLearnerMLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim=512, num_classes=2, dropout=0.5):
+    def __init__(self, input_dim, num_classes=2, dropout=0.5):
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
+            # Layer 1: input_dim → 1024
+            nn.Linear(input_dim, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim, hidden_dim // 2),
-            nn.BatchNorm1d(hidden_dim // 2),
+            
+            
+            # Layer 2: 1024 → 512
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(hidden_dim // 2, num_classes)
+            
+            # Layer 3: 512 → 256
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            
+            # Layer 4: 256 → 128
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            
+            # Output: 128 → 2
+            nn.Linear(128, num_classes)
         )
         
     def forward(self, x):
